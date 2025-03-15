b19204730c1fea556f0bff50d18e3c78446e9af6
devsecops-edu-vn <138289285+devsecops-edu-vn@users.noreply.github.com>
2025-03-15 15:23:05 +0700

    Đếm lại và tính lại số file của Makefile

--- sfolcount.sh
+++ sfolcount.sh
@@ -328,9 +328,15 @@ yacc_bison_total=$yacc_bison_count0
 
 ###############################################################################
 
-# 30. Makefile - Thường viết bằng Bash shell
+# 30. Makefile, Makefile.in, .mk - Thường viết bằng Bash shell
 
-makefile_count=$(find $1 -type f -name "Makefile" | wc -l)
+makefile_count0=$(find $1 -type f -name "Makefile" | wc -l)
+
+makefile_count1=$(find $1 -type f -name "Makefile.in" | wc -l)
+
+makefile_count2=$(find $1 -type f -name "*.mk" | wc -l)
+
+makefile_total=`expr $makefile_count0 + $makefile_count1 + $makefile_count2`
 
 ###############################################################################
 
@@ -361,7 +367,7 @@ TOTAL_FILES_LANGS=`expr $ada_total + $assembly_total + $awk_total + $bash_total 
   $haskell_total + $java_total + $lex_flex_total + $lisp_scheme_total + $modula3_total + \
   $node_javascript_total + $objectivec_total + $pascal_total + $perl_total + $php_total + \
   $python_total + $ruby_total + $rust_total + $sed_total + $sql_total + $tcl_total + $yacc_bison_total + \
-  $makefile_count + $headerfile_count + $cmake_total`
+  $makefile_total + $headerfile_count + $cmake_total`
 
 ###############################################################################
 
@@ -524,7 +530,7 @@ if [ $yacc_bison_total -gt 0 ] ; then
   echo "Yacc/Bison:         $yacc_bison_total file(s) - ${yacc_bison_rate}%"
 fi
 
-if [ $makefile_count -gt 0 ] ; then
+if [ $makefile_total -gt 0 ] ; then
   makefile_rate=`echo "scale=6 ; ($makefile_count/$TOTAL_FILES_LANGS)*100" | bc`
   echo "Makefile:           $makefile_count file(s) - ${makefile_rate}%"
 fi
